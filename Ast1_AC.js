30927dff90d7c1f9f56dad3bb23e6c42eeab5dc6
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2016-09-19 17:22:26 -0400

    Initial commit

---
+++ Ast1_AC.js
@@ -0,0 +1,77 @@
+/*
+* Anthony Courchesne
+* 260688650
+* anthony.courchesne@mail.mcgill.ca
+* COMP302 - Assignment 1
+* Prof. C. Verbrugge
+*
+* 19 Septembre 2016
+*/
+//-----------Question 1---------------
+function cXXXr(ads){
+    if(ads.length==1){
+        if(ads == 'a'){
+            return function (list){
+                return car(list);
+            }
+        }
+        if(ads == 'd'){
+            return function (list){
+                return cdr(list);
+            }
+        }
+   }
+   if(ads.charAt(0)=='a'){
+    return function (list){
+        return car(cXXXr(ads.slice(1,ads.length))(list));
+    }
+   }
+   if(ads.charAt(0)=='d'){
+    return function (list){
+        return cdr(cXXXr(ads.slice(1,ads.length))(list));
+    }
+   }
+}
+
+var ls = cons('v',cons('w',cons('x',cons('y',cons('z',null)))));
+var cadr = cXXXr('ad');
+var caddr = cXXXr('add');
+
+//document.writeln("cadr: "+cadr(ls));
+//document.writeln("caddr: "+caddr(ls));
+//document.write(cXXXr("adddd")(ls));
+
+//---------------Question 2 ------------------
+
+//this function find a string then returns its path
+//Debth-first search
+function makeXXX(tree,s){
+    var left = car(tree);
+    var right = cdr(tree);
+    if(!isList(left)){
+        if(left===s){   //left is the searched string
+            return "a";
+        }
+    }else{  //left is a list
+        var result = makeXXX(left,s);
+        if(result!=null){   //string found in left
+            return result+"a";
+        }
+    }
+    //string not found in left side
+    if(isList(right)){
+        var result = makeXXX(right,s);
+        if(result!=null){   //string found in right
+            return result+"d";
+        }
+    }
+    //right is null
+    return null;
+}
+
+var t = rndTree(0.6);
+document.write(show(t.tree));
+document.write("<BR><BR>");
+document.write("Target: "+t.target);
+document.write("<BR><BR>");
+document.write(makeXXX(t.tree,t.target));
