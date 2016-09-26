c7bafd11590ea2db4d2e2fa53905a9613e6bfc3e
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2016-09-26 18:29:04 -0400

    test for BuildString

--- Ast1_AC.js
+++ Ast1_AC.js
@@ -8,6 +8,7 @@
 * 19 Septembre 2016
 */
 //-----------Question 1---------------
+console.log("test1");
 function cXXXr(ads){
     if(ads.length==1){
         if(ads == 'a'){
@@ -112,5 +113,26 @@ function wosh(param){
     }
 
 }
+
 var t = rndTree(0.6);
-document.write(show(t.tree));
+//document.write(show(t.tree));
+console.log("t");
+
+function BuildString(str,i,pStack){
+        //return the left part
+        var strConstruct;
+        var stack = pStack;
+        var character = str.charAt(i);
+        if(character==40)
+            stack++;
+        if(character==41)
+            stack--;
+        if(stack==0 && i!=0){
+            return character;
+        }
+        var strConstruct = character+BuildString(str,i+1,stack);
+}
+
+console.log("test");
+//console.log(BuildString("((test(tt))te)(not)",0,0));
+
